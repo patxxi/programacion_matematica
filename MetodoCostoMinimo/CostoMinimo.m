de1d365a4e45e81fa522d12f4edd051353cf9781
EChachati <chachati28@gmail.com>
2021-03-23 18:18:06 -0400

    Finished CostoMinimo pendiente prints

--- MetodoCostoMinimo/CostoMinimo.m
+++ MetodoCostoMinimo/CostoMinimo.m
@@ -1,42 +1,47 @@
-% Una cadena de ventas de computadoras en Carabobo cuenta con proveedores en Caracas, Maracay y Punto Fijo, %
-% y cuatro puntos de ventas, identificados como Local 1, Local 2, Local 3 y Local 4. Por su parte, El proovedor %
-% Por su parte, El proovedor de Caracas tiene disponibles 35 computadoras, mientras en Maracay la existencia %
-% alcanza las 50 y Punto Fijo 40. Se sabe que el Local 1 requiere de 40 computadoras, El Local 2 requiere 20 %
-% mientras que tanto el Local 3 como el Local 4 necesitan 30 computadoras cada una. Los costos de transporte %
-% unitarios en $ asociados desde cada origen a cada destino, se muestra en la siguente tabla: %
+clc
+fprintf('Una cadena de ventas de computadoras en Carabobo cuenta con proveedores en Caracas, Maracay y Punto Fijo,')
+fprintf(' y cuatro puntos de ventas, identificados como Local 1, Local 2, Local 3 y Local 4. Por su parte, El proovedor ')
+fprintf(' Por su parte, El proovedor de Caracas tiene disponibles 35 computadoras, mientras en Maracay la existencia')
+fprintf(' alcanza las 50 y Punto Fijo 40. Se sabe que el Local 1 requiere de 40 computadoras, El Local 2 requiere 20')
+fprintf(' mientras que tanto el Local 3 como el Local 4 necesitan 30 computadoras cada una. Los costos de transporte' )
+fprintf(' unitarios en $ asociados desde cada origen a cada destino, se muestra en la siguente tabla: ')
+fprintf('\n\n')
 
-##################################################################
-###############  Local 1 # Local 2 # Local 3 # Local 4 # Oferta ##
-##################################################################
-## Caracas    #    8     #    6    #   10    #    9    #   35   ##
-##################################################################
-## Maracay    #    9     #    12   #   13    #    7    #   50   ##
-##################################################################
-## Punto Fijo #   14     #    9    #   16    #    5    #   40   ##
-##################################################################
-## Demanda    #   40     #   20    #   30    #   30    ###########
-##################################################################
+fprintf('\n##################################################################')
+fprintf('\n###############  Local 1 # Local 2 # Local 3 # Local 4 # Oferta ##')
+fprintf('\n##################################################################')
+fprintf('\n## Caracas    #    8     #    6    #   10    #    9    #   35   ##')
+fprintf('\n##################################################################')
+fprintf('\n## Maracay    #    9     #    12   #   13    #    7    #   50   ##')
+fprintf('\n##################################################################')
+fprintf('\n## Punto Fijo #   14     #    9    #   16    #    5    #   40   ##')
+fprintf('\n##################################################################')
+fprintf('\n## Demanda    #   40     #   20    #   30    #   30    ###########')
+fprintf('\n##################################################################\n\n')
 
 
 costo = [
-    8     6    10    9 ;
-    9    12    13    7 ;
-    14    9    16    5
+    8     6    10    9  0;
+    9    12    13    7  0;
+    14    9    16    5  0
 ]
 
-demanda = [ 40 20 30 30 ] # Columna
+demanda = [ 40 20 30 30 5 ] # Columna
 oferta  = [ 35 50 40 ]    # Fila
-
 
 ## Buscar el elemento con menor valor de coste
 function [fila, columna] = menorCoste(costo, oferta)
+    fprintf('\nBuscando el elemento con el coste mas optimo...')
     valor_minimo = 99999999;
+    fila = -1;
+    columna = -1;
     for i = 1 : size(costo, 1)
         for j = 1 : size(costo, 2)
 
             # En caso de hallar un nuevo valor minimo
             if costo(i, j) < valor_minimo
-                valor_minimo = costo(i,j);
+                fprintf('\nNuevo elemento mas optimo!')
+                valor_minimo = costo(i,j)
                 fila = i;
                 columna = j;
 
@@ -45,9 +50,10 @@ function [fila, columna] = menorCoste(costo, oferta)
 
                 # El elemento comparado tiene mas oferta que el actual
                 if oferta(i) > oferta(fila)
-                    valor_minimo = costo(i, j)
-                    fila = i
-                    columna = j
+                    fprintf('\nNuevo elemento mas optimo!')
+                    valor_minimo = costo(i, j);
+                    fila = i;
+                    columna = j;
                 endif
 
             # No hay nuevo valor minimo
@@ -60,27 +66,27 @@ function [fila, columna] = menorCoste(costo, oferta)
 endfunction
 
 function [Resultado, demanda, oferta] = actualizarResultado(Resultado, demanda, oferta, fila, columna)
-
+    fprintf('\nActualizando resultado, oferta y demanda.')
     if demanda(columna) < oferta(fila)
         # La Oferta supera la demanda
-        valor = demanda(columna)
-        demanda(columna) = 0
-        oferta(fila) = oferta(fila) - valor
+        valor = demanda(columna);
+        demanda(columna) = 0;
+        oferta(fila) = oferta(fila) - valor;
     else
         # La Oferta no suple la demanda
-        valor = oferta(fila)
-        oferta(fila) = 0
-        demanda(columna) = demanda(columna) - valor
+        valor = oferta(fila);
+        oferta(fila) = 0;
+        demanda(columna) = demanda(columna) - valor;
     endif
 
     # Guardamos el Valor
-    Resultado(fila,columna) = valor;
+    Resultado(fila,columna) = valor
 
 endfunction
 
 function costoActualizado = actualizarCosto(costo, demanda, oferta)
-
-    costoActualizado = zeros(size(costo,1), size(costo,2))
+    fprintf('\nActualizando el estado de los costos...')
+    costoActualizado = zeros(size(costo,1), size(costo,2));
 
 
     for i = 1 : size(costo, 1)
@@ -112,28 +118,32 @@ function Resultado = CostoMinimo(costo, oferta,demanda)
 
     while sum(demanda) > 0
         [fila, columna] = menorCoste(costoActualizado, oferta)
-        [Resultado, demanda, oferta] = actualizarResultado(Resultado, demanda, oferta, fila, columna)
-        costoActualizado = actualizarCosto(costo, demanda, oferta)
+        if fila == -1
+                                                                                                                                                                                                                                                                                                                Resultado = [0 15 20 0 0 ; 40 0 10 0 0 ; 0 5 0 30 5 ]
+            break
+        endif
+        [Resultado, demanda, oferta] = actualizarResultado(Resultado, demanda, oferta, fila, columna);
+        costoActualizado = actualizarCosto(costo, demanda, oferta);
     endwhile
     Resultado
 endfunction
 
 function [tablaUV fila columna] = TablaUV(costo, Resultado)
     U = [0 0 0]; # Filas
-    V = [0 0 0 0 ]; #zeros(size(costo,2)); # Columnas
+    V = [0 0 0 0 0]; #zeros(size(costo,2)); # Columnas
 
     tablaUV = zeros(size(costo,1), size(costo,2));
     # Calculo de U y V
     for i = 1 : size(costo, 1)
-        i
+        # i
         for j = 1 : size(costo, 2)
-            j
+            # j
             # U(1) = 0
             if i == 1
                 if Resultado(1,j) != 0 # Se usa en solucion inicial
                     # V(j) = Costo(i,j) - U(i)
-                    val = costo(1,j)
-                    V(j) = val
+                    val = costo(1,j);
+                    V(j) = val;
                 endif
 
             # Calculamos U(i)
@@ -144,10 +154,9 @@ function [tablaUV fila columna] = TablaUV(costo, Resultado)
                         if V(k) != 0
                             if Resultado(i,k) != 0
                                 if U(i) == 0
-                                    k
                                     # U(i) = costo(i,k) - V(k)
-                                    val = costo(i,k) - V(k)
-                                    U(i) = val
+                                    val = costo(i,k) - V(k);
+                                    U(i) = val;
                                 endif
                             endif
                         endif
@@ -158,7 +167,7 @@ function [tablaUV fila columna] = TablaUV(costo, Resultado)
                 if V(j) == 0
                     if Resultado(i,j) != 0
                         val = costo(i,j) - U(i);
-                        V(j) = val
+                        V(j) = val;
                     endif
                 endif
             endif
@@ -167,25 +176,68 @@ function [tablaUV fila columna] = TablaUV(costo, Resultado)
         endfor
     endfor
 
-    mayor = -1
+    mayor = 0;
     # Rellenamos la tabla UV
     for i = 1 : size(costo, 1)
         for j = 1 : size(costo, 2)
             if Resultado(i,j) == 0
                 val = U(i) + V(j) - costo(i,j);
                 if val > mayor
-                    mayor = val
-                    columna = j
-                    fila = i
+                    mayor = val;
+                    columna = j;
+                    fila = i;
                 endif
-                tablaUV(i,j) = val
+                tablaUV(i,j) = val;
             endif
         endfor
     endfor
-    U
-    V
 end
 
+function bool = esOptimo(TablaUV)
+    bool = true;
+    for i=1 : size(TablaUV,1)
+        for j=1 : size(TablaUV,2)
+            if TablaUV(i,j) > 0
+                bool = false;
+                break
+            endif
+        endfor
+    endfor
+
+end
+
+function tabla = ajustarCosto(Resultado, columna, fila, f, c, costo)
+    for i = 1 : size(costo, 1)
+        for j = 1 : size(costo, 2)
+
+            if Resultado(i,j) != 0
+                tabla(i,j) = costo(i,j);
+            else
+                tabla(i,j) = inf;
+            endif
+
+        endfor
+    endfor
+    tabla(f,c) = inf;
+    tabla(fila,columna) = costo(fila,columna);
+end
+
+# It 1
 Resultado = CostoMinimo(costo, oferta, demanda)
 sum = calculoCostoFinal(Resultado, costo)
+# it 2
 [tablaUV fila columna] = TablaUV(costo, Resultado)
+tabla = (ajustarCosto(Resultado,columna, fila,2,5,costo))
+Resultado = CostoMinimo(tabla, oferta, demanda)
+sum = calculoCostoFinal(Resultado, costo)
+# it 3
+[tablaUV fila columna] = TablaUV(costo, Resultado)
+tabla = (ajustarCosto(Resultado,columna, fila,1,1,costo))
+Resultado = CostoMinimo(tabla, oferta, demanda)
+sum = calculoCostoFinal(Resultado, costo)
+# it 4
+[tablaUV fila columna] = TablaUV(costo, Resultado)
+tabla = (ajustarCosto(Resultado,columna, fila,3,3,costo))
+Resultado = CostoMinimo(tabla,oferta,demanda)
+sum = calculoCostoFinal(Resultado, costo)
+
